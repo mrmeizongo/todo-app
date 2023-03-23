745a53c84ec3d6b3e78d4a90da33950daff9aa82
Jamal Meizongo <mrmeizongo@outlook.com>
2023-03-22 21:55:11 -0400

    Added coming soon banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import "./App.css";
+
 function App() {
 	return (
-		<div className="">
+		<div className="App">
 			<h1>Todo App</h1>
+			<p>Coming soon. Please check back again later.</p>
 		</div>
 	);
 }
